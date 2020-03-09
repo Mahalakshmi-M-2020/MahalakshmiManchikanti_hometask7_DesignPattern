cfe506a4b82ea4c3811a5586b84fa12a0b0739b8
Mahalakshmi-M-2020 <60378469+Mahalakshmi-M-2020@users.noreply.github.com>
2020-03-09 22:10:15 +0530

    Add files via upload

---
+++ hometask7/src/main/java/hometask7/BuilderPatternMain.java
@@ -0,0 +1,10 @@
+package hometask7;
+
+public class BuilderPatternMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hometask7/src/main/java/hometask7/ProxyPatternMain.java
@@ -0,0 +1,12 @@
+package hometask7;
+
+public class ProxyPatternMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Image image = new ProxyImage("test_10mb.jpg");
+	      image.display(); 
+	      System.out.println("");
+	      image.display(); 	
+	   }
+	}
